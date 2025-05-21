75a46895ea2a7cb6d34196a7109a624fcff9abbe
Sehaj004 <anshpreetkathpal1234@gmail.com>
2025-05-22 01:31:56 +1000

    Initial commit with Selenium TDD test

---
+++ src/test/java/edu/ontrack/TaskSubmissionTest.java
@@ -0,0 +1,34 @@
+package edu.ontrack;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TaskSubmissionTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/anshpreetsingh/Downloads/chromedriver-mac-arm64/chromedriver"); // change this
+        driver = new ChromeDriver();
+        driver.navigate().to("file:////Users/anshpreetsingh/Downloads/java_projects/ontrack-task-submission/src/pages/submit-task.html"); 
+    }
+
+
+    @Test
+    public void testValidTaskSubmission() {
+        driver.findElement(By.id("studentId")).sendKeys("12345");
+        driver.findElement(By.id("taskFile")).sendKeys("/Users/anshpreetsingh/Downloads/java_projects/ontrack-task-submission/src/pages/task1.docx");
+        driver.findElement(By.id("submitBtn")).click();
+
+        WebElement message = driver.findElement(By.id("successMessage"));
+        assertEquals("Task submitted successfully!", message.getText());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
